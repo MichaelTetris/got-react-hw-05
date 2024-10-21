6edcc4669daf377c0183430b6ba279eb6b5eb5e2
Mykhailo Tkachenko <125746441+MichaelTetris@users.noreply.github.com>
2024-10-21 14:03:11 +0300

    input field

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,17 @@ const HomePage = () => {
   return (
     <div>
       <h2>Trending today</h2>
-      <h3>Home</h3>
+      <ul>
+        <li>1 sdffdsfsffdffsdf</li>
+        <li>1 sdffdsfsffdff sdad sdf</li>
+        <li>1 sdffsds sdsd dsfsffdffsdf</li>
+        <li>1 3333 ds fdsfsffdffsdf</li>
+        <li>1 sdffdsfsf uytu yuytyu fdffsdf</li>
+        <li>fsffdffsdf</li>
+        <li>1 sdffdsf sff sdf</li>
+        <li>5 sdffdsfsffdffsdf</li>
+        <li>18 g fdfd sdffdsfsffdffsdf</li>
+      </ul>
     </div>
   );
 };
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,7 +1,45 @@
+import css from "./MoviePage.module.css";
+import { ErrorMessage, Field, Form, Formik } from "Formik";
+
+import { useState } from "react";
+
 const MoviesPage = () => {
+  const [movies, setMovies] = useState([]);
+
   return (
     <div>
-      <h2>Moooooovies Page</h2>
+      <div className={css.searchBar}>
+        <Formik
+        /* initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validationSchema={SearchMovies} */
+        >
+          <Form>
+            <label className={css.formLabel}>
+              <Field
+                className={css.searchInput}
+                type="text"
+                name="searchTerm"
+                placeholder="Search movies"
+              />
+            </label>
+            <button className={css.searchBtn} type="submit">
+              Search
+            </button>
+            <br />
+            <ErrorMessage
+              className={css.formErrorMessage}
+              name="searchTerm"
+              component="span"
+            />
+          </Form>
+        </Formik>
+      </div>
+      {/* <div>
+        <ul className={css.moviesList}>
+          <MovieList trendingMovies={movies} />
+        </ul>
+      </div> */}
     </div>
   );
 };
