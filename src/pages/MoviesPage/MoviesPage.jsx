316861b06ac99b116b091c8e13f0ddf8f862db69
Mykhailo Tkachenko <125746441+MichaelTetris@users.noreply.github.com>
2024-10-27 22:45:24 +0200

    page by moviespage create

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,18 +1,52 @@
 import css from "./MoviePage.module.css";
-import { ErrorMessage, Field, Form, Formik } from "Formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { useSearchParams } from "react-router-dom";
+// import { lazy } from "react";
+
+import { fetchSearchMovies } from "../Services/Api.jsx";
+
+const SearchMovies = Yup.object().shape({
+  searchTerm: Yup.string()
+    .min(2, "Too Short! Min 2 symbols.")
+    .max(50, "Too Long! Max 50 symbols.")
+    .required("Required! Enter any word..."),
+});
+
+const initialValues = {
+  searchTerm: "",
+};
 
 const MoviesPage = () => {
   const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const query = searchParams.get("query");
+
+  useEffect(() => {
+    if (query === null) return;
+    const searchMoviesBySearchValue = async () => {
+      const data = await fetchSearchMovies(query);
+      setMovies(data.results);
+    };
+    searchMoviesBySearchValue();
+  }, [query]);
+
+  const handleSubmit = (values, actions) => {
+    setSearchParams({ query: values.searchTerm });
+    actions.resetForm();
+  };
 
   return (
     <div>
       <div className={css.searchBar}>
         <Formik
-        /* initialValues={initialValues}
+          initialValues={initialValues}
           onSubmit={handleSubmit}
-          validationSchema={SearchMovies} */
+          validationSchema={SearchMovies}
         >
           <Form>
             <label className={css.formLabel}>
